4b4a3d389a526d95e7fd1cd3789c7348681b803d
msagca <51697294+msagca@users.noreply.github.com>
2023-01-19 11:04:48 +0300

    fix formal proofs

--- solutions/1-two-sum/s1.v
+++ solutions/1-two-sum/s1.v
@@ -1,6 +1,6 @@
 module s1 #(
     parameter DATA_WIDTH = 2,
-    parameter ARRAY_SIZE = 2 ** DATA_WIDTH  // to avoid hash collisions
+    parameter ARRAY_SIZE = 2 ** DATA_WIDTH
 ) (
     input clk,
     input rst,
@@ -8,21 +8,20 @@ module s1 #(
     input wire number_valid,
     input wire number_last,
     input wire signed [DATA_WIDTH-1:0] target,
-    output wire [$clog2(ARRAY_SIZE)-1:0] index1,
-    output wire [$clog2(ARRAY_SIZE)-1:0] index2,
-    output wire index_valid
+    output reg [$clog2(ARRAY_SIZE)-1:0] index1,
+    output reg [$clog2(ARRAY_SIZE)-1:0] index2,
+    output reg index_valid = 0
 );
   reg [$clog2(ARRAY_SIZE)-1:0] number_index;
+  wire collision;
   wire signed [DATA_WIDTH-1:0] complement;
   wire [$clog2(ARRAY_SIZE)-1:0] complement_index;
   wire complement_valid;
   wire clear_cache;
-  reg [$clog2(ARRAY_SIZE)-1:0] index1_int;
-  reg [$clog2(ARRAY_SIZE)-1:0] index2_int;
-  reg index_valid_int;
+  reg last_received;
   initial begin
-    number_index <= 0;
-    index_valid_int <= 0;
+    number_index  <= 0;
+    last_received <= 0;
   end
   always @(posedge clk)
     if (rst) number_index <= 0;
@@ -30,55 +29,84 @@ module s1 #(
       if (number_last) number_index <= 0;
       else number_index <= number_index + 1;
   always @(posedge clk)
-    if (rst) index_valid_int <= 0;
+    if (rst) last_received <= 0;
+    else last_received <= number_valid & number_last;
+  always @(posedge clk)
+    if (rst) index_valid <= 0;
     else if (index_valid) begin
-      if (clear_cache) index_valid_int <= 0;
-    end else if (complement_valid) begin
-      index1_int <= number_index;
-      index2_int <= complement_index;
-      index_valid_int <= 1;
+      if (last_received) index_valid <= 0;
+    end else if (number_valid && complement_valid) begin
+      index1 <= number_index;
+      index2 <= complement_index;
+      index_valid <= 1;
     end
   hashmap #(
       .KEY_WIDTH  (DATA_WIDTH),
       .VALUE_WIDTH($clog2(ARRAY_SIZE)),
-      .CACHE_SIZE (ARRAY_SIZE)
+      .CACHE_SIZE (ARRAY_SIZE),
+      .OVERWRITE  (1)
   ) hashmap_i (
       .clk(clk),
       .rst(rst),
       .write_key(number),
       .write_value(number_index),
       .write_request(number_valid),
+      .collision(collision),
       .read_key(complement),
       .read_value(complement_index),
       .read_response(complement_valid),
       .clear_cache(clear_cache)
   );
-  assign complement = target - number;
+  assign complement  = target - number;
   assign clear_cache = number_valid && number_last;
-  assign index1 = index1_int;
-  assign index2 = index2_int;
-  assign index_valid = index_valid_int;
 `ifdef FORMAL
+  localparam _STATE_FIRST = 0;
+  localparam _STATE_SECOND = 1;
+  localparam _STATE_LAST = 2;
   reg _past_valid;
   (* anyconst *) reg signed [DATA_WIDTH-1:0] _target1;
   (* anyconst *) reg signed [DATA_WIDTH-1:0] _number1;
   reg signed [DATA_WIDTH-1:0] _number2;
   reg [$clog2(ARRAY_SIZE)-1:0] _index1;
   reg [$clog2(ARRAY_SIZE)-1:0] _index2;
+  reg [1:0] _state;
+  reg [1:0] _state_next;
   initial begin
     _past_valid <= 0;
+    _state <= _STATE_FIRST;
     _number2 <= _target1 - _number1;
   end
   always @(posedge clk) _past_valid <= 1;
-  always @* assume (target == _target1);
-  always @* if (number_index == ARRAY_SIZE - 1) assume (number_last);
-  always @(posedge clk) if (number_valid && number == _number1) _index1 <= number_index;
-  always @(posedge clk) if (number_valid && number == _number2) _index2 <= number_index;
-  // verify that the indices are correct (ignore the order)
   always @(posedge clk)
-    if (index_valid)
-      assert ((index1 == _index1 && index2 == _index2) || (index1 == _index2 && index2 == _index1));
-  // cover the case where a solution has been found
+    if (rst) _state <= _STATE_FIRST;
+    else if (number_valid)
+      if (number_last) _state <= _STATE_FIRST;
+      else _state <= _state_next;
+  always @*
+    case (_state)
+      _STATE_FIRST: _state_next = _STATE_SECOND;
+      default: _state_next = _STATE_LAST;
+    endcase
+  always @(posedge clk) if (_state == _STATE_FIRST) _index2 <= number_index;
+  always @(posedge clk) if (_state == _STATE_SECOND) _index1 <= number_index;
+  // assumptions
+  always @* assume (target == _target1);
+  always @(posedge clk) if (_past_valid && index_valid && !last_received) assume ($stable(_state));
+  always @(posedge clk) if (_past_valid && $stable(number)) assume ($stable(number_last));
+  always @(posedge clk) if (!number_valid) assume (!number_last);
+  always @* if (number_index == ARRAY_SIZE - 1) assume (number_last);
+  always @* if (!index_valid) assume (_state < 2);
+  always @*
+    case (_state)
+      _STATE_FIRST: assume (number == _number2);
+      _STATE_SECOND: assume (number == _number1);
+      default: assume (number != _number1 && number != _number2);
+    endcase
+  // verify that the indices are correct if a solution has been found
+  always @* if (index_valid) assert (index1 == _index1 && index2 == _index2);
+`ifdef TOP
+  // cover the case where a solution exists
   always @* cover (index_valid);
+`endif
 `endif
 endmodule
--- solutions/common/hashmap.v
+++ solutions/common/hashmap.v
@@ -1,13 +1,15 @@
 module hashmap #(
     parameter KEY_WIDTH   = 4,
     parameter VALUE_WIDTH = 4,
-    parameter CACHE_SIZE  = 4
+    parameter CACHE_SIZE  = 4,
+    parameter OVERWRITE   = 1
 ) (
     input wire clk,
     input wire rst,
     input wire [KEY_WIDTH-1:0] write_key,
     input wire [VALUE_WIDTH-1:0] write_value,
     input wire write_request,
+    output wire collision,
     input wire [KEY_WIDTH-1:0] read_key,
     output wire [VALUE_WIDTH-1:0] read_value,
     output wire read_response,
@@ -16,24 +18,22 @@ module hashmap #(
   localparam ADDR_WIDTH = $clog2(CACHE_SIZE);
   reg [KEY_WIDTH-1:0] key_mem[0:CACHE_SIZE-1];
   reg [VALUE_WIDTH-1:0] value_mem[0:CACHE_SIZE-1];
-  reg valid_mem[0:CACHE_SIZE-1];
+  reg [CACHE_SIZE-1:0] valid_mem;
   wire [ADDR_WIDTH-1:0] write_ptr;
   wire [ADDR_WIDTH-1:0] read_ptr;
   integer i;
-  initial begin
-    if (CACHE_SIZE > 2 ** KEY_WIDTH)
-      $warning("Cache size is greater than the number of unique keys!");
-    for (i = 0; i < CACHE_SIZE; i = i + 1) valid_mem[i] <= 0;
-  end
+  initial for (i = 0; i < CACHE_SIZE; i = i + 1) valid_mem[i] <= 0;
   assign write_ptr = hash(write_key);
   always @(posedge clk)
     if (rst || clear_cache) for (i = 0; i < CACHE_SIZE; i = i + 1) valid_mem[i] <= 0;
     else valid_mem[write_ptr] <= write_request;
   always @(posedge clk)
-    if (write_request) begin
-      key_mem[write_ptr]   <= write_key;
-      value_mem[write_ptr] <= write_value;
-    end
+    if (write_request)
+      if (OVERWRITE == 1 || !valid_mem[write_ptr]) begin
+        key_mem[write_ptr]   <= write_key;
+        value_mem[write_ptr] <= write_value;
+      end
+  assign collision = write_request && valid_mem[write_ptr];
   assign read_ptr = hash(read_key);
   assign read_value = value_mem[read_ptr];
   assign read_response = valid_mem[read_ptr];
@@ -46,34 +46,45 @@ module hashmap #(
   (* anyconst *) reg [KEY_WIDTH-1:0] _key1;
   (* anyconst *) reg [VALUE_WIDTH-1:0] _val1;
   reg [ADDR_WIDTH-1:0] _hash1;
-  initial _past_valid <= 0;
+  initial begin
+    _past_valid <= 0;
+    _collision_occured <= 0;
+    _hash1 <= hash(_key1);
+  end
   always @(posedge clk) _past_valid <= 1;
+  always @(posedge clk)
+    if (rst || clear_cache) _collision_occured <= 0;
+    else if (write_request)
+      if (write_key == _key1) _collision_occured <= 0;
+      else if (hash(write_key) == _hash1) _collision_occured <= 1;
+  // assumptions
+  always @*
+    if (!_collision_occured && valid_mem[_hash1])
+      assume (key_mem[_hash1] == _key1 && value_mem[_hash1] == _val1);
+  always @* if (write_key == _key1) assume (write_value == _val1);
   // verify that data is available at the output one clock cycle after a successful write
   always @(posedge clk)
-    if (_past_valid && $past(!rst && !clear_cache && write_request) && read_key == $past(write_key)) begin
+    if (_past_valid && $past(
+            !rst && !clear_cache && write_request
+        ) && read_key == $past(
+            write_key
+        )) begin
       assert (read_response);
       assert (read_value == $past(write_value));
     end
   // verify that read queries fail when the cache is empty
   always @(posedge clk) if (_past_valid && $past(rst || clear_cache)) assert (!read_response);
   // verify that a query with a known key returns the correct value unless a collision occured
-  initial begin
-    _collision_occured <= 0;
-    _hash1 <= hash(_key1);
-  end
-  always @(posedge clk)
-    if (rst || clear_cache) _collision_occured <= 0;
-    else if (write_request)
-      if (write_key == _key1) _collision_occured <= 0;
-      else if (hash(write_key) == _hash1) _collision_occured <= 1;
   always @*
-    if (!_collision_occured && valid_mem[_hash1])
-      assert (key_mem[_hash1] == _key1 && value_mem[_hash1] == _val1);
-  always @* if (write_key == _key1) assume (write_value == _val1);
-  always @(posedge clk)
     if (!_collision_occured && read_key == _key1 && read_response)
       assert (read_value == _val1);
-	// cover the case of a successful query
-	always @* cover(read_response);
+`ifdef TOP
+  // cover the case of a successful query
+  always @* cover (read_response);
+  // cover the case of full cache
+  always @* cover (&valid_mem);
+  // cover the case of a collision
+  always @* cover (collision);
+`endif
 `endif
 endmodule
